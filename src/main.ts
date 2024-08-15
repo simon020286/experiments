2de8347d70c265c4b7f73dda46d587446a07aba4
Code with heart <codewithheart@gmail.com>
2024-08-15 15:20:06 +0200

    Slider vanilla

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,22 @@ const gra = function(min: number, max: number) {
 }
 
 const ul = document.querySelector<HTMLUListElement>('ul')!;
+const text = document.querySelector<HTMLDivElement>('#log')!;
 
-for (let i = 0; i < 10; i++) {
+const log = function() {
+	text.innerHTML = `Scroll top: ${Math.ceil(ul.scrollTop)}<br>
+	Scroll height: ${ul.scrollHeight}<br>
+	End: ${ul.scrollHeight - ul.offsetHeight} < ${Math.round(ul.scrollTop)}<br>
+	End: ${ul.scrollHeight - ul.offsetHeight <= Math.round(ul.scrollTop)}`;
+}
+
+ul.addEventListener('scroll', log);
+
+for (let i = 0; i < 15; i++) {
 	const li = document.createElement('li');
-	li.style.minHeight = `${gra(30, 80)}px`;
+	li.style.minHeight = `${gra(60, 80)}px`;
 	li.innerHTML = `${i}`;
 	ul.appendChild(li);
 }
+
+log();
