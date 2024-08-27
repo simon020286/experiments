18d2c1f3714f672ec9d38b00d4cbdd34a702f58d
simon020286 <jemisi@hotmail.it>
2024-08-27 09:32:25 +0200

    New api

--- slider-angular/src/app/slider/slider.directive.ts
+++ slider-angular/src/app/slider/slider.directive.ts
@@ -1,50 +1,88 @@
-import { contentChildren, Directive, effect, ElementRef, HostListener, inject, input, OnInit, output, viewChildren } from '@angular/core';
+import { computed, contentChildren, Directive, effect, ElementRef, HostListener, inject, input, model, OnInit, output, untracked, viewChildren } from '@angular/core';
 
 @Directive({
   selector: '[sliderItem]',
   standalone: true,
+  host: {
+    'style': 'scroll-snap-align: start; box-sizing: border-box'
+  }
 })
 export class SliderItem {
-  private element = inject(ElementRef).nativeElement;
+  element = inject(ElementRef).nativeElement as HTMLElement;
 
   scrollIntoView() {
-    this.element.scrollIntoView();
+    this.element.scrollIntoView({
+      block: 'start',
+      inline: 'start',
+      behavior: 'smooth'
+    });
+  }
+
+  get position() {
+    return this.element.getBoundingClientRect();
   }
 }
 
 @Directive({
   selector: '[slider]',
   standalone: true,
+  host: {
+    '[style.overflow]': `'scroll'`,
+    '[style.scrollbar-width]': `'none'`,
+    '[style.scroll-snap-type]': `snapType()`
+  },
 })
 export class SliderDirective {
-  private elRef = inject(ElementRef);
+  private sliderElement = inject(ElementRef).nativeElement as HTMLElement;
   elements = contentChildren(SliderItem);
 
   isEnd = output<boolean>();
   isStart = output<boolean>();
-  firstVisible = input(0);
+  firstVisible = model(0);
+  navigate = model<'next' | 'prev' | undefined>();
+
+  currentElement = model<SliderItem>();
+  direction = input<'horizontal' | 'vertical'>('vertical');
+  
+  private snapType = computed(() => this.direction() === 'horizontal' ? 'x mandatory' : 'y mandatory');
+  private topLeft = computed(() => this.direction() === 'horizontal' ? 'left' : 'top');
+  private scrollTopLeft = computed(() => this.direction() === 'horizontal' ? 'scrollLeft' : 'scrollTop');
+  private scrollHeightWidth = computed(() => this.direction() === 'horizontal' ? 'scrollWidth' : 'scrollHeight');
+  private offsetHeightWidth = computed(() => this.direction() === 'horizontal' ? 'offsetWidth' : 'offsetHeight');
 
   constructor() {
     effect(() => {
-      if (this.firstVisible() < this.elements().length) {
-        this.elements()[this.firstVisible()].scrollIntoView();
+      const currentElement = untracked(() => this.currentElement());
+      const elements = untracked(() =>  this.elements());
+      if (this.firstVisible() < elements.length && elements[this.firstVisible()] !== currentElement) {
+         this.elements()[this.firstVisible()].scrollIntoView();
       }
     });
   }
 
-  @HostListener('scroll')
+  @HostListener('scrollend')
   onScroll() {
     this.isStart.emit(this.calculateStart());
     this.isEnd.emit(this.calculateEnd());
+    for (let index = 0; index < this.elements().length; index++) {
+      const element = this.elements()[index];
+      if (this.calculateDistance(element.position) <= 0) {
+        this.currentElement.set(element);
+        this.firstVisible.set(index);
+        break;
+      }
+    }
   }
 
   calculateStart() {
-    return this.elRef.nativeElement.scrollTop === 0;
+    return this.sliderElement[this.scrollTopLeft()] === 0;
   }
 
   calculateEnd() {
-    const ul = this.elRef.nativeElement;
-    return ul.scrollHeight - ul.offsetHeight <= Math.round(ul.scrollTop);
+    return this.sliderElement[this.scrollHeightWidth()] - this.sliderElement[this.offsetHeightWidth()] <= Math.round(this.sliderElement[this.scrollTopLeft()]);
   }
 
+  calculateDistance(el: DOMRect) {
+    return Math.ceil(this.sliderElement.getBoundingClientRect()[this.topLeft()] - el[this.topLeft()]);
+  }
 }
